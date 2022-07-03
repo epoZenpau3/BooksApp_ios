09225d9e4833df7da1a622037c56b15aae80cd5d
Nikitin Dmitrij <epozee@gmail.com>
2022-07-03 20:51:12 +0300

    playing with button

--- BooksApp/ViewController/ShowBooksViewController.swift
+++ BooksApp/ViewController/ShowBooksViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 class ShowBooksViewController: UIViewController {
 
+    lazy var myButton: UIButton = {
+        let button = UIButton()
+        button.configuration = .gray()
+        button.configuration?.title = "Hello"
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
+        return button
+    }()
+    
+    fileprivate func setupConstraints() {
+        myButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        myButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        myButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        myButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(myButton)
+        myButton.backgroundColor = .brown
+        setupConstraints()
+    }
+
+    @objc func pressed() {
+    
         ApiService.getBooks { booksFrom in
             guard let books = booksFrom else  { return }
             for book in books {
